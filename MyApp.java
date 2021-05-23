89ea588183b91a25020f6eecee721abc02f3278c
sugurunnba <sugurunnba7@gmail.com>
2021-05-23 18:12:06 +0900

    first

---
+++ MyApp.java
@@ -0,0 +1,10 @@
+public class MyApp {
+  public static void main(String[] args){
+    String msg = "Hello World Again!";
+    System.out.println(msg);
+  }
+}
+// コンパイル
+// javac MyApp.java
+// 実行
+// java MyApp
